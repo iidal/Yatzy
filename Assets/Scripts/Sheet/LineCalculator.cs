0024c366be0757bba145d9847897ed9afbe53715
Iida <iida.lintuaho@gmail.com>
2019-09-03 23:03:34 +0300

    all lines can be played. still missing calculating the upper bonus and if player gets yatzy more than once

--- Assets/Scripts/Sheet/LineCalculator.cs
+++ Assets/Scripts/Sheet/LineCalculator.cs
@@ -11,12 +11,16 @@ public class LineCalculator
     /// </summary>
 
     static int[] nums = new int[5];
+    
     static string type;
 
     public static void StartCalculating(SingleLine[] lines, int[] results) {
-
+        //Setting up the dice row for checking it
         results.CopyTo(nums, 0);
+       
 
+ 
+        // checking line
         foreach (SingleLine sl in lines) {
             type = sl.lineType;
 
@@ -34,11 +38,23 @@ public class LineCalculator
                     case "pair":
                         CalculateAmountOfSameDices(sl, 2);
                         break;
+                    case "2pairs":
+                        CalculateTwoPairs(sl);
+                        break;
                     case "3X":
                         CalculateAmountOfSameDices(sl, 3);
                         break;
                     case "4X":
                         CalculateAmountOfSameDices(sl, 4);
+                        break;
+                    case "house":
+                        CalculateFullHouse(sl);
+                        break;
+                    case "sStraight":
+                        CalculateStraight(sl, 4);
+                        break;
+                    case "lStraight":
+                        CalculateStraight(sl, 5);
                         break;
                     case "yatzy":
                         CalculateYatzy(sl);
@@ -81,6 +97,7 @@ public class LineCalculator
             amount += i;
         }
 
+        //if the highest value in tempdict is equal or higher than the checked number, the dice row meets the requirements for giving points
         foreach (KeyValuePair<int, int> kvp in tempDict) {
             if (kvp.Value >= threeOrFour) {
                 sl.SetDicePoints(amount);
@@ -89,11 +106,138 @@ public class LineCalculator
         }
         //HOX if there are enough of the same number code beyond this point(return) will not be executed
         sl.SetDicePoints(0);
+    }
 
-        //foreach (KeyValuePair<int, int> kvp in tempDict) {
-        //    Debug.Log(kvp.Key +" "+ kvp.Value);
-        //}
-   
+    //two pairs
+    static void CalculateTwoPairs(SingleLine sl) {
+        int amount = 0; //points to be given two pairs
+        Dictionary<int, int> tempDict = new Dictionary<int, int>();
+        foreach (int i in nums)
+        {
+            if (tempDict.ContainsKey(i))
+            {
+                tempDict[i] += 1;
+            }
+            else
+            {
+                tempDict.Add(i, 1);
+            }
+            amount += i;
+        }
+        //if tempdict has more than 3 kvps, two pairs is impossible, give 0 points
+        if (tempDict.Count > 3) {
+            sl.SetDicePoints(0);
+            return;
+        }
+        //if the highest kvp value is 4 or more, the dice row is for example 11112, which counts as two pairs (2 sets of 1 1)
+        foreach (KeyValuePair<int, int> kvp in tempDict)
+        {
+            if (kvp.Value >= 4)
+            {
+                sl.SetDicePoints(amount);
+                return;
+            }
+        }
+        //finally checking for two pairs, both with different digit (or a full house)
+        int amountOfPairs = 0;
+        foreach (KeyValuePair<int, int> kvp in tempDict)
+        {
+            if (kvp.Value ==2 || kvp.Value ==3)
+            {
+                amountOfPairs++;
+            }
+        }
+        if (amountOfPairs >= 2)
+        {
+            sl.SetDicePoints(amount);
+        }
+        //if dice row is for example one pair or three same 
+        else {
+            sl.SetDicePoints(0);
+        }
+
+    }
+
+    //full house
+    static void CalculateFullHouse(SingleLine sl) {
+        Dictionary<int, int> tempDict = new Dictionary<int, int>();
+        foreach (int i in nums)
+        {
+            if (tempDict.ContainsKey(i))
+            {
+                tempDict[i] += 1;
+            }
+            else
+            {
+                tempDict.Add(i, 1);
+            }
+        }
+        //checking if there is a 3x AND a pair, if yes, full house, else 0 points
+        bool threeSame = false;
+        bool pair = false;
+        foreach (KeyValuePair<int, int> kvp in tempDict)
+        {
+            if (kvp.Value == 3)
+            {
+                threeSame = true;
+            }
+            if (kvp.Value == 2) {
+                pair = true;
+            }
+        }
+        if (threeSame && pair)
+        {
+            sl.SetOtherPoints(true);
+        }
+        else {
+            sl.SetOtherPoints(false);
+        }
+
+    }
+
+    //small and large straight
+    static void CalculateStraight(SingleLine sl, int numsInRow) {
+        //numsinrow= how many dices needs to be in a row for a straight
+        //small = 4 digits in row
+        //large = 5 digits in row
+        List<int> numsList = new List<int>(); //make nums into list so Sort() can be used
+
+        //not adding duplicate values to numslist, they are not needed for checking straight
+        foreach (int i in nums) {
+
+            bool exists = numsList.Exists(digit => digit == i);
+            if (!exists)
+            {
+                numsList.Add(i);
+            }
+        }
+        numsList.Sort();
+
+        //there has to be enough different values for a straight to be possible (longer the list more different numbers)
+        if (numsList.Count < numsInRow) {
+            sl.SetOtherPoints(false);
+            return;
+        }
+        //if enought different values, proceed
+
+        int count = 1; //how many values are in row (cant be zero there is always the first dice)
+        for (int i = 1; i<numsList.Count;i++) {
+            //the previuous value plus 1 should be same as current value
+            if (numsList[i] == (numsList[i -1] + 1))
+            {
+                count++;
+            }
+            else {//if not, start from one
+                count = 1;
+            }
+        }
+        if (count >= numsInRow) {
+            sl.SetOtherPoints(true);
+        }
+        else
+        {
+            sl.SetOtherPoints(false);
+        }
 
     }
 
