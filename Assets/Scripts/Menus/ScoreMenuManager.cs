acee9b7c09a43b168d4e717fd28f5ef58c9b5335
Iida <iida.lintuaho@gmail.com>
2019-10-01 03:15:52 +0300

    top 10 list gets sorted

--- Assets/Scripts/Menus/ScoreMenuManager.cs
+++ Assets/Scripts/Menus/ScoreMenuManager.cs
@@ -9,6 +9,7 @@ public class ScoreMenuManager : MonoBehaviour
     public TextMeshProUGUI scoreBoardText;
     string scoresText;
     List<SavedResult> resultList = new List<SavedResult>();
+    public TextMeshProUGUI[] textBlocks;
 
     void Awake()
     {
@@ -16,21 +17,53 @@ public class ScoreMenuManager : MonoBehaviour
     }
 
     void LoadScoreBoard(){
-        scoresText = "";
-        scoreBoardText.text = scoresText;
+
 
         resultList = SaveLoad.Load();
         
-        if(resultList!=null){
-            foreach(SavedResult sr in resultList){
-                scoresText += sr.playerName.ToString() + " - " + sr.result.ToString() + "\n";
 
+        
+
+        //resultList = SortResults(resultList);
+
+        int i = 0;
+        if(resultList!=null){
+            //resultList.Sort();
+            resultList.Sort((x, y) => y.result.CompareTo(x.result));
+            int resultsCount = resultList.Count;
+            Debug.Log(resultsCount);
+            foreach(TextMeshProUGUI t in textBlocks){
+                if(i < resultsCount){
+                    t.text = (i+1).ToString() + ". " +resultList[i].playerName.ToString() + " - " + resultList[i].result.ToString() + "\n";
+                }
+                else{
+                    t.text = (i+1).ToString() + ".";
+                }
+
+                i++;
             }
-            scoreBoardText.text = scoresText;
+    
         }
         else{
             Debug.Log("no scores");
         }
     }
+    List <SavedResult> SortResults(List<SavedResult> res){
+        List<SavedResult> temp = new List<SavedResult> ();
 
+        // foreach(SavedResult sr in res){
+        //     if(temp == null){   //adding the firts one
+        //         temp.Add(sr);
+        //     }
+        //     else
+        //     {
+        //         for(int i = 0; i<res.Count; i++){
+                   
+        //         }
+        //     }
+
+        // }
+
+        return temp;
+    }
 }
--- Assets/Scripts/Tools/Saving/SavedResult.cs
+++ Assets/Scripts/Tools/Saving/SavedResult.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-[System.Serializable] 
-public class SavedResult
+[System.Serializable]
+public class SavedResult: IComparable<SavedResult>
 {
     public string playerName;
     public int result;
 
-    public SavedResult(){
-        playerName = "";
-        result = 0;
+//public string playerName {get;set};
+//public int result {get;set;}
+    public SavedResult()
+    {
+       playerName = "";
+         result = 0;
 
+    }
+    public int CompareTo(SavedResult other)
+    {
+        return result.CompareTo(other.result);
     }
 }
