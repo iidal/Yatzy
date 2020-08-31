9cd0b0d95417fcd94dd8a59f4c53b4602a249c6f
Iida <iida.lintuaho@gmail.com>
2020-08-31 21:19:18 +0300

    long waited git fix

--- Assets/Scripts/Tools/EditorComment.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EditorComment : MonoBehaviour
-{
-    [TextArea(3,10)]
-    public string MyTextArea;
-}
